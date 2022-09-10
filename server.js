954125c161fb1b8b42035580fa985c46099fd0e8
Spi051997 <dcodeacedemy97@gmail.com>
2022-09-10 15:56:23 +0530

    Depployment try

--- server.js
+++ server.js
@@ -1,17 +1,21 @@
 const express=require('express');
 const app=express();
 const dotenv=require('dotenv');
+const cors=require('cors');
 const dbconnection=require('./config/mongoose');
 const Bookroutes=require('./routes/Bookroutes');
 dbconnection
  dotenv.config();
+
+ PORT=process.env.PORT;
  
+ app.use(cors());
  app.use(express.json());
 
  app.use('/api/book',Bookroutes);
 
 
-PORT=process.env.PORT;
+
 app.listen(PORT,()=>{
     console.log(`Server is up at ${PORT}`);
 })
